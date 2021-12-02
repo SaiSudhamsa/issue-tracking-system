4d8125e737a87cd3ddf81d2860011b8dbbf1df0a
SaiSudhamsa <potru.saisudhamsa@gmail.com>
2021-12-02 18:40:37 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//import Sidebar from "./Components/Sidebar";
-//import Topbar from "./Components/Topbar";
 import "./index.css";
 import Auth from "./Components/login/Authentication";
 
-/*function Overview(){
-  return (
-    <div className="Overview">
-      <Topbar />
-      <Sidebar />
-    </div>
-    );
+import NewUser from "./views/NewUser";
+import Sidebar from "./Components/Sidebar";
+import Topbar from "./Components/Topbar";
+import AddTicket from "./Components/AddTicket";
+
+function Overview() {
+    return (
+        <div>
+            <div>
+                <div> <Topbar /> </div>
+                <div> <Sidebar /> </div>
+                <div className="content">
+                    <AddTicket />
+                </div>
+                
+            </div>
+        </div>
+    )
 }
-*/
-ReactDom.render(<Auth />, document.getElementById('root'));
+
+ReactDom.render(<AddTicket />, document.getElementById('root'));
